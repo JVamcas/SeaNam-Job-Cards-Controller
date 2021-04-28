28a3e71f977a587ce2fc714fa21bf8a8b5503878
Petrus Kambala <kmbpet001@myuct.ac.za>
2021-04-28 16:58:26 +0200

    job card table added

--- src/main/java/com/jvmtechs/controllers/home/HomeController.kt
+++ src/main/java/com/jvmtechs/controllers/home/HomeController.kt
@@ -1,25 +1,50 @@
 package com.jvmtechs.controllers.home
 
+import com.jvmtechs.controllers.AbstractModelTableController
 import com.jvmtechs.controllers.AbstractView
+import com.jvmtechs.model.JobCard
+import javafx.collections.ObservableList
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.TableView
 import javafx.scene.control.TitledPane
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
+import kotlinx.coroutines.Job
 import tornadofx.*
 
-class HomeController: AbstractView("") {
+class HomeController: AbstractModelTableController<JobCard>("") {
 
     override val root: BorderPane by fxml("/view/HomeView.fxml")
+    private lateinit var scrollPane: ScrollPane
     private val jobCardView : GridPane by fxml("/view/JobCardView.fxml")
     private val jobCardSearch: GridPane by fxml("/view/jobCardSearch.fxml")
 
+
+    private lateinit var tableView: TableView<JobCard>
+
     init {
         root.apply {
-
+            scrollPane = root.center as ScrollPane
             left{
                 vbox (10.0){
                     style = "-fx-background-color: #045A6B"
                    add(jobCardView)
                     add(jobCardSearch)
+                }
+            }
+
+            center{
+                tableView = tableview(modelList){
+                    smartResize()
+                    prefWidthProperty().bind(scrollPane.widthProperty())
+                    prefHeightProperty().bind(scrollPane.heightProperty())
+
+                    items = modelList
+
+                    placeholder = label("No Job Cards here yet.")
+
+                    columns.add(indexColumn)
+
                 }
             }
         }
@@ -28,5 +53,9 @@ class HomeController: AbstractView("") {
     override fun onDock() {
         super.onDock()
         currentStage?.scene?.stylesheets?.add("style/style.css")
+    }
+
+    override suspend fun loadModels(): ObservableList<JobCard> {
+        return observableListOf()
     }
 }
